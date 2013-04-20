19a6ff6adc4af3e131ae7bf0c278aad184471de8
Steve Marshall <steve@nascentguruism.com>
2013-04-20 22:04:46 +0100

    Add TODO.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,6 @@ override['couch_db']['config']['httpd']['secure_rewrites'] = false
 override['couch_db']['config']['httpd']['bind_address'] = "0.0.0.0"
 
 # Allow our npm couch repo to respond to vanity URLs when on a fort
+# TODO: Move these to a separate thing so we only use them when setting up a fort?
 override['couch_db']['config']['vhosts']['npm.fort'] = "/registry/_design/ui/_rewrite"
 override['couch_db']['config']['vhosts']['npm-registry.fort'] = "/registry/_design/scratch/_rewrite"
