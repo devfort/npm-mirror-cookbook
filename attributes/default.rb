585c210a7cfaffb52f661fc905535eb1de496ea5
Steve Marshall <steve@nascentguruism.com>
2013-04-20 21:38:31 +0100

    Support vanity URLs for use on forts.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,6 @@
 override['couch_db']['config']['httpd']['secure_rewrites'] = false
 override['couch_db']['config']['httpd']['bind_address'] = "0.0.0.0"
+
+# Allow our npm couch repo to respond to vanity URLs when on a fort
+override['couch_db']['config']['vhosts']['npm.fort'] = "/registry/_design/ui/_rewrite"
+override['couch_db']['config']['vhosts']['npm-registry.fort'] = "/registry/_design/scratch/_rewrite"
