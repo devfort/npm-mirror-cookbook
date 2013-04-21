fcb4cdae09f37fe0b1df900d584fb3e732aacfe6
Steve Marshall <steve@nascentguruism.com>
2013-04-21 20:47:42 +0100

    Focus on couch + curl for now.

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,23 +12,11 @@ execute 'Force couch to start cleanly' do
   notifies :start, "service[couchdb]", :immediate
 end
 
-include_recipe "nodejs"
-include_recipe "nodejs::npm"
-
 package "curl"
-package "git"
 
 execute "Create registry" do
   command <<-EOF
     curl -X PUT http://localhost:#{node.couch_db.config.httpd.port}/registry
-  EOF
-end
-
-execute "Install couchapp and semver" do
-  command <<-EOF
-  npm install couchapp -g
-  npm install couchapp
-  npm install semver
   EOF
 end
 
@@ -41,24 +29,36 @@ execute "Replicate npm's couchdb" do
   EOF
   returns 28
 end
-
-git "/srv/npmjs.org" do
-  repository "https://github.com/isaacs/npmjs.org.git"
-  enable_submodules true
-end
-
-execute "Sync the registry-rewriter and search UI" do
-  cwd "/srv/npmjs.org"
-  command <<-EOF
-  couchapp push registry/app.js http://localhost:#{node.couch_db.config.httpd.port}/registry
-  couchapp push www/app.js http://localhost:#{node.couch_db.config.httpd.port}/registry
-  EOF
-  # HACK: couchapp errors, but pushes successfully
-  # events.js:72
-  #         throw er; // Unhandled 'error' event
-  #               ^
-  # Error: spawn ENOENT
-  #     at errnoException (child_process.js:945:11)
-  #     at Process.ChildProcess._handle.onexit (child_process.js:736:34)
-  returns 8
-end
+# 
+# include_recipe "nodejs"
+# include_recipe "nodejs::npm"
+# package "git"
+# 
+# execute "Install couchapp and semver" do
+#   command <<-EOF
+#   npm install couchapp -g
+#   npm install couchapp
+#   npm install semver
+#   EOF
+# end
+# 
+# git "/srv/npmjs.org" do
+#   repository "https://github.com/isaacs/npmjs.org.git"
+#   enable_submodules true
+# end
+# 
+# execute "Sync the registry-rewriter and search UI" do
+#   cwd "/srv/npmjs.org"
+#   command <<-EOF
+#   couchapp push registry/app.js http://localhost:#{node.couch_db.config.httpd.port}/registry
+#   couchapp push www/app.js http://localhost:#{node.couch_db.config.httpd.port}/registry
+#   EOF
+#   # HACK: couchapp errors, but pushes successfully
+#   # events.js:72
+#   #         throw er; // Unhandled 'error' event
+#   #               ^
+#   # Error: spawn ENOENT
+#   #     at errnoException (child_process.js:945:11)
+#   #     at Process.ChildProcess._handle.onexit (child_process.js:736:34)
+#   returns 8
+# end
