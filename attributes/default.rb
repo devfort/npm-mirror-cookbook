ecaa82f5d38a9e7ed41f44f925e271d9971fa2e5
Steve Marshall <steve@nascentguruism.com>
2013-04-26 06:37:47 +0100

    Fix stupid typo.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-defaults['npm_mirror']['user'] = 'fort'
+default['npm_mirror']['user'] = 'fort'
 
 override['couch_db']['config']['httpd']['secure_rewrites'] = false
 override['couch_db']['config']['httpd']['bind_address'] = "0.0.0.0"
